884c748849ebecc2779e966e5994307f79bfe6c6
Benoit <dufresne.benoit@gmail.com>
2018-08-06 21:09:40 +0000

    waiting until a temperature has been sensed to show a chart, filling data with that temperature instead of zeros. using temp rather than avgtemp

--- chart.py
+++ chart.py
@@ -21,7 +21,9 @@ class Chart(object):
     logger.info('GUI constructor')
     self._state = state
     self._len = int(span / refresh_rate)
-    self._data = [self._state['curtemp'] for x in xrange(self._len)]
+    while self._state['temp'] == 0.0:
+      time.sleep(0.1)
+    self._data = [self._state['temp'] for x in xrange(self._len)]
     self._target = [self._state['settemp'] for x in xrange(self._len)]
     self._refresh_rate = refresh_rate
 
@@ -79,7 +81,7 @@ class Chart(object):
     crashed = False
     try:
       while not crashed:
-        temp = self._state['avgtemp']
+        temp = self._state['temp']
         self.add_temp(temp)
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
